d3ec60c02d15af48f67e3f503c239a4500ca42c4
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-09-23 19:59:05 -0600

    onChange and onClick added

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
 
-const EvenAndOdd = () => {
+class EvenAndOdd extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { 
+            evenArray: [],
+            oddArray: [],
+            userInput: ""
+         }
+    };
+    handleChange(value) {
+        this.setState({userInput: value});
+    }
+
+
+
+   
+
+
+    render () {
+        
+        const onClick = () => {
+            console.log("Clicked");
+        }
+
+        const onChange = (event) => {
+            this.handleChange(event.target.value)
+        }
+
     return (
-        <p>Even and Odd</p>
+        <div className="puzzleBox evenAndOddPB">
+            <h4 value="Evens and Odds">Even and Odds</h4>
+            <input  onChange={onChange}className="inputLine" ></input>
+            <button onClick={onClick} className="confirmationButton">Confirm</button>
+            <span className="resultsBox"></span>
+            <span className="resultsBox"></span>
+
+        </div>
+
+  
+
     )
+}
 }
 
 export default EvenAndOdd;
